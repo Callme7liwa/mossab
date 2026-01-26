e0687da4295c54b5b530c01b8c85bc22eabe73ae
Your Name <you@example.com>
2026-01-26 13:16:57 +0100

    updates

--- server/ecosystem.config.cjs
+++ server/ecosystem.config.cjs
@@ -2,7 +2,7 @@ module.exports = {
   apps: [
     {
       name: 'aura-backend',
-      cwd: '/home/ubuntu/aura-estates-insight/server',
+      cwd: '/home/ubuntu/ecosystem.config.cjs/server',
       script: 'index.js',
       instances: 1,
       exec_mode: 'fork',
